5f639b1febea1403d8c4c3500e2de3ee53675f1a
huseyinC1876 <huseyinc1876@gmail.com>
2023-06-02 09:48:21 +0200

    Image superVaisseau et Galactus

--- src/main/java/fr/montreuil/iut/CakarCassirame/vue/EnnemiGalactusBossVue.java
+++ src/main/java/fr/montreuil/iut/CakarCassirame/vue/EnnemiGalactusBossVue.java
@@ -4,6 +4,6 @@ import javafx.scene.layout.Pane;
 
 public class EnnemiGalactusBossVue extends EnnemieVue{
     public EnnemiGalactusBossVue(Pane pane) {
-        super(pane, "src/main/resources/fr/montreuil/iut/CakarCassirame/Galactus.jpg");
+        super(pane, "src/main/resources/fr/montreuil/iut/CakarCassirame/galactusFOND32.png");
     }
 }
--- src/main/java/fr/montreuil/iut/CakarCassirame/vue/EnnemiSuperVaisseauSpatialVue.java
+++ src/main/java/fr/montreuil/iut/CakarCassirame/vue/EnnemiSuperVaisseauSpatialVue.java
@@ -4,6 +4,6 @@ import javafx.scene.layout.Pane;
 
 public class EnnemiSuperVaisseauSpatialVue extends EnnemieVue{
     public EnnemiSuperVaisseauSpatialVue(Pane pane) {
-        super(pane, "src/main/resources/fr/montreuil/iut/CakarCassirame/ennemiExtraterrestre32.png");
+        super(pane, "src/main/resources/fr/montreuil/iut/CakarCassirame/superVaisseauSpatial32.png");
     }
 }
