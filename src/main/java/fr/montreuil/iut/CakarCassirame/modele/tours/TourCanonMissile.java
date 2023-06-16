7530afd927417d5329b1bb7d69fdc363db2ed21c
victoirecassirame <victoirecassirame@gmail.com>
2023-06-16 16:37:48 +0200

    nettoyage code VerifMort et verifFinPartie

--- src/main/java/fr/montreuil/iut/CakarCassirame/modele/tours/TourCanonMissile.java
+++ src/main/java/fr/montreuil/iut/CakarCassirame/modele/tours/TourCanonMissile.java
@@ -1,13 +1,13 @@
 package fr.montreuil.iut.CakarCassirame.modele.tours;
 
 import fr.montreuil.iut.CakarCassirame.modele.Environnement;
+import fr.montreuil.iut.CakarCassirame.modele.Parametre;
 
 
 public class TourCanonMissile extends TourTeteChercheuse{
 
     public TourCanonMissile(Environnement environnement, int x, int y) {
         super(environnement, x, y, 65);
-
     }
 
 }
